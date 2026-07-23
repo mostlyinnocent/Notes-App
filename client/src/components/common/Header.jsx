a64ad467e008bab725981d62ac4e2d24cd32b6b5
mostlyinnocent <debdebraj453@gmail.com>
2026-07-23 22:26:28 +0530

    Remove placeholder text from Header

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='header h-15 w-full px-5 border-b border-b-neutral-400/50'>Header</div>
+    <div className='header h-15 w-full px-5 border-b border-b-neutral-400/50'></div>
   )
 }
 
